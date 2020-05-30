f011195a8c5099b19dcd2c8049c46343b388d091
Balazs Heizer <heizer.balazs@gmail.com>
2020-05-30 23:07:59 +0200

    refactor opc-ua client

--- OPC-UA-client/opc-ua-client.py
+++ OPC-UA-client/opc-ua-client.py
@@ -3,23 +3,24 @@ import os
 from asyncua import Client
 from KafkaProducer.SubscriptionHandler import SubscriptionHandler
 
-SERVER_URL = os.environ.get('OPC_UA_SERVER_URL')
-print(SERVER_URL)
-NAMESPACE_URI = os.environ.get('NAMESPACE_URI')
-print(NAMESPACE_URI)
-SERVER_URL = 'opc.tcp://0.0.0.0:4840/freeopcua/server/'
-NAMESPACE_URI = 'http://plant_simulation'
+if os.environ.get('IN_DOCKER'):
+    SERVER_URL = os.environ.get('OPC_UA_SERVER_URL')
+    NAMESPACE_URI = os.environ.get('NAMESPACE_URI')
+else:
+    SERVER_URL = 'opc.tcp://0.0.0.0:4840/freeopcua/server/'
+    NAMESPACE_URI = 'http://plant_simulation'
+
 EVENT_TYPE_PATH = ['0:Types', '0:EventTypes', '0:BaseEventType', '2:MachineCycleEvent']
 OBJECTS_PATH = ['0:Objects']
 
-async def create_client(url, loop):
-    async with Client(url, loop=loop) as client:
+async def run_client(url):
+    async with Client(url) as client:
         root = client.get_root_node()
         print(root)
         idx = await client.get_namespace_index(NAMESPACE_URI)
 
-        subscription = await client.create_subscription(1, subscription_handler)
         subscription_handler = SubscriptionHandler()
+        subscription = await client.create_subscription(1, subscription_handler)
         handles = []
 
         event_type = await root.get_child(EVENT_TYPE_PATH)
@@ -37,7 +38,7 @@ async def create_client(url, loop):
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(create_client(SERVER_URL,loop))
+        loop.run_until_complete(run_client(SERVER_URL))
     except KeyboardInterrupt:
         pass
     finally:
